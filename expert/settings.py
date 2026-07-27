4e4c10a0c32cebb8bccc3a23673c8b780e371fc7
srahbarzahid <rahbarzahid96@gmail.com>
2026-07-27 13:27:42 +0530

    Hardcode ALLOWED_HOSTS = ['*']

--- expert/settings.py
+++ expert/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-t3p!)@%ga9(ddc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
