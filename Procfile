58379e577ed5107409c6606741b3e5ff1b4345a0
srahbarzahid <rahbarzahid96@gmail.com>
2026-07-27 12:37:09 +0530

    Configure Django for Supabase and Render deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn expert.wsgi:application
--- expert/settings.py
+++ expert/settings.py
@@ -74,15 +74,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'expert.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
+
 
 
 # Password validation
